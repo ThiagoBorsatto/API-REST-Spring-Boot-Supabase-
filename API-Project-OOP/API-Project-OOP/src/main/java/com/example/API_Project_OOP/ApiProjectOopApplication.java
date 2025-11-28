f9df2acd47869b0e3e0e468ed1b22bf6f34dff8f
Thiago Borsatto <thiagoborsattodutra14@gmail.com>
2025-11-28 13:38:35 -0300

    New Project: upload the SpringBoot file to the team work together.

---
+++ API-Project-OOP/API-Project-OOP/src/main/java/com/example/API_Project_OOP/ApiProjectOopApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_Project_OOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjectOopApplication.class, args);
+	}
+
+}
---
+++ API-Project-OOP/API-Project-OOP/src/test/java/com/example/API_Project_OOP/ApiProjectOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_Project_OOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
