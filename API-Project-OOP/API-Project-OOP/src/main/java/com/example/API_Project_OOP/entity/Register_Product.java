98a01ae3bbba218fde74393abcc4c8d0c1fac0df
KAUÊ PRIOLI DIMAS <kaue_dimas@estudante.sesisenai.org.br>
2025-12-02 19:14:19 -0300

    atualizado entity e repository

---
+++ API-Project-OOP/API-Project-OOP/src/main/java/com/example/API_Project_OOP/entity/Register_Product.java
@@ -0,0 +1,47 @@
+package com.example.API_Project_OOP.entity;
+
+import java.math.BigDecimal;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id_product")
+@Table(name = "Register_Product")
+public class Register_Product {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name= "Id_Product")
+    private Long id_product;
+
+    @Column(name= "Description", length= 100, nullable= false)
+    private String description;
+
+    @Column(name= "Product_Value", precision= 10, scale= 2, nullable=false)
+    private BigDecimal product_value;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name= "Category_Id_Category", nullable= false)
+    private Category id_category;
+
+    public Register_Product() {
+    }
+
+    public Register_Product(Category id_category, String description, Long id_product, BigDecimal product_value) {
+        this.id_category = id_category;
+        this.description = description;
+        this.id_product = id_product;
+        this.product_value = product_value;
+    }
+}
---
+++ API-Project-OOP/API-Project-OOP/src/main/java/com/example/API_Project_OOP/repository/Register_ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.API_Project_OOP.repository;
+
+import com.example.API_Project_OOP.entity.Register_Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Register_ProductRepository extends JpaRepository<Register_Product, Long>{
+}
